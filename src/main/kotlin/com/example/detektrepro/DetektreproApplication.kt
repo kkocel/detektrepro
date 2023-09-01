13441d02300b7a9b5c65dcd1a665dc479d9bdbe0
Krzysztof Kocel <krzysztof.kocel@gmail.com>
2023-09-01 11:47:06 +0200

    init

---
+++ src/main/kotlin/com/example/detektrepro/DetektreproApplication.kt
@@ -0,0 +1,11 @@
+package com.example.detektrepro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DetektreproApplication
+
+fun main(args: Array<String>) {
+    runApplication<DetektreproApplication>(args = args)
+}
---
+++ src/test/kotlin/com/example/detektrepro/DetektreproApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.detektrepro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DetektreproApplicationTests {
+
+    @Test
+    fun contextLoads() {
+        println("loads")
+    }
+}
